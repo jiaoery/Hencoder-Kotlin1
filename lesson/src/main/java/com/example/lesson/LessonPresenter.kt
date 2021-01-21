dddb126d684e47cbc6a355211c4bea7907bfcfd9
jiaoery <373105324@qq.com>
2021-01-22 00:22:31 +0800

    完成编写

---
+++ lesson/src/main/java/com/example/lesson/LessonPresenter.kt
@@ -0,0 +1,58 @@
+package com.example.lesson
+
+import com.example.core.http.EntityCallback
+import com.example.core.http.HttpClient.get
+import com.example.core.utils.Utils.toast
+import com.example.lesson.entity.Lesson
+import com.google.gson.reflect.TypeToken
+import java.util.*
+
+/**
+ * ClassName: LessonPresenter<br/>
+ * Description: TODO Description. <br/>
+ * date: 2021/1/22 0:12<br/>
+ *
+ * @author Administrator
+ * @version V1.0.0
+ * @since JDK 1.8
+ */
+class LessonPresenter {
+
+    companion object{
+        const val LESSON_PATH = "lesson"
+    }
+
+    private var activity: LessonActivity? = null
+
+    constructor(activity: LessonActivity?) {
+        this.activity = activity
+    }
+
+    private var lessons: List<Lesson> = ArrayList()
+
+    private val type = object : TypeToken<List<Lesson?>?>() {}.type
+
+    fun fetchData() {
+        get(LESSON_PATH, type, object : EntityCallback<List<Lesson>> {
+            override fun onSuccess(lessons: List<Lesson>) {
+                this@LessonPresenter.lessons = lessons
+                activity!!.runOnUiThread { activity!!.showResult(lessons) }
+            }
+
+            override fun onFailure(message: String?) {
+                activity!!.runOnUiThread { toast(message) }
+            }
+        })
+    }
+
+    fun showPlayback() {
+        val playbackLessons: MutableList<Lesson> = ArrayList()
+        for (lesson in lessons) {
+            if (lesson.state === Lesson.State.PLAYBACK) {
+                playbackLessons.add(lesson)
+            }
+        }
+        activity!!.showResult(playbackLessons)
+    }
+
+}
--- lesson/src/main/java/com/example/lesson/entity/Lesson.kt
+++ lesson/src/main/java/com/example/lesson/entity/Lesson.kt
@@ -1,60 +1,24 @@
-package com.example.lesson.entity;
+package com.example.lesson.entity
 
-public class Lesson {
-    public enum State {
+class Lesson(var date: String, var content: String, var state: State) {
+    enum class State {
         PLAYBACK {
-            public String stateName() {
-                return "有回放";
+            override fun stateName(): String {
+                return "有回放"
             }
         },
-
         LIVE {
-            public String stateName() {
-                return "正在直播";
+            override fun stateName(): String {
+                return "正在直播"
             }
         },
-
         WAIT {
-            public String stateName() {
-                return "等待中";
+            override fun stateName(): String {
+                return "等待中"
             }
         };
 
-        public abstract String stateName();
-    }
-
-    private String date;
-    private String content;
-    private State state;
-
-    public Lesson(String date, String content, State state) {
-        this.date = date;
-        this.content = content;
-        this.state = state;
-    }
-
-    public State getState() {
-        return state;
-    }
-
-    public void setState(State state) {
-        this.state = state;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
+        abstract fun stateName(): String
     }
 
 }
